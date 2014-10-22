b3f7772903940fda3985e6ca8f8cf02292a3f2ee
mohssine.amhachi <mohssine.amhachi@gmail.com>
2014-10-22 20:40:51 +0200

    Modify : classe App de test de fichier ihm_metronome.fxml

--- metronome/src/main/java/fr/istic/aocproject/metronome/App.java
+++ metronome/src/main/java/fr/istic/aocproject/metronome/App.java
@@ -1,13 +1,26 @@
 package fr.istic.aocproject.metronome;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/ihm_metronome.fxml"));
+        primaryStage.setScene(new Scene(root, 500, 350));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
